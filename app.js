6920bc97b5d4d99d2f171a6454be19508f458a68
wruiz <wilson.ruiz@eltransporte.org>
2020-01-26 17:29:09 -0500

    Iniciando proyecto node

---
+++ app.js
@@ -0,0 +1,21 @@
+const argv = require ('./config/yargs').argv
+const colors = require('colors');
+const {crearArchivo, listarTabla} = require('./multiplicar/multiplicar')
+
+let comando =argv._[0]
+
+switch (comando) {
+  case 'listar':
+    console.log('Listar')
+    listarTabla(argv.base, argv.limite)
+  break;
+  case 'crear':
+    console.log('Crear')
+    crearArchivo(argv.base, argv.limite)
+    .then(archivo => console.log(`Archivo creado: ${archivo.green}`))
+    .catch((err) => {console.log(err)})
+  break;
+  default:
+    console.log('Comando no reconocido')
+
+}
